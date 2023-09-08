bf29cd50fcb4a2959a503e23afc276c166b0de00
Leandro A. Dickmann <102850262+LeandroDickmann@users.noreply.github.com>
2023-09-07 21:03:00 -0400

    Page_d’exercice
    Création de page d’exercice, et feuille de style avec des propriétés spécifiques.

---
+++ exercice.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alex+Brush&family=Bebas+Neue&family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Exercice - Cours PHP</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Cours PHP</h1>
+        <h2>Visualisation des Exercices</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+        
+        </div>
+    </main>
+    <footer class="rodape">
+        Développé par Leandro Dickmann &copy; <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
